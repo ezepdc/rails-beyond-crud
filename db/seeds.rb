edcc3def9bdf98902716a34d7823f51144256619
Ezequiel <epuyosdacosta@gmail.com>
2022-11-10 12:07:58 +0100

    add chef name model route+view

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,8 @@ puts 'Creating restaurants...'
   restaurant = Restaurant.create(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address,
-    rating: rand(1..5)
+    rating: rand(1..5),
+    chef_name: ["Charly", "Josephine", "Rupert", "James"].sample
   )
   puts "Restaurant with id: #{restaurant.id} has been created"
 end
